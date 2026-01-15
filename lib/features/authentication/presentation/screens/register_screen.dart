650c32d6376a1159281406f51fb5b6420c9b5e66
Jeremiah Karanja <jeremiahkaranja584@gmail.com>
2026-01-15 21:24:48 +0300

    updated login

--- lib/features/authentication/presentation/screens/register_screen.dart
+++ lib/features/authentication/presentation/screens/register_screen.dart
@@ -1,3 +1,4 @@
+///home/hp/JERE/AutoNest-frontend/lib/features/authentication/presentation/screens/register_screen.dart
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
